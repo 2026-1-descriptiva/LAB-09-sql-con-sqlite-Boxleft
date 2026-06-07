8925e9309ee02a2944e571b303cae074729fe4fc
Boxleft <jmunozre@unal.edu.co>
2026-06-07 18:26:58 -0500

    Update pregunta_13.sql

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -44,7 +44,7 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
 SELECT K0,
-       ROUND(AVG(c12), 2) AS "avg(c12)"
+       AVG(c12) AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
 GROUP BY K0
